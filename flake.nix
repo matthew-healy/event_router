4523c9e235883b1f2105f6c9946174776ccd2f13
Matthew Healy <matthew@liamhealy.xyz>
2023-09-09 11:56:27 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "playing with event routing in Go";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        preCommitHook =
+          ({ doFullLint ? false }: pre-commit-hooks.lib.${system}.run {
+            src = self;
+
+            hooks = {
+              nixpkgs-fmt.enable = true;
+
+              gofmt.enable = true;
+              gotest.enable = true;
+            };
+          });
+
+        eventRouterPkg = pkgs.buildGoModule {
+          pname = "event-router";
+          version = "v0";
+
+          src = ./.;
+
+          vendorHash = null;
+          # vendorSha256 = pkgs.lib.fakeSha256;
+        };
+      in
+      {
+        packages = rec {
+          event-router = eventRouterPkg;
+
+          default = event-router;
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${(preCommitHook {}).shellHook}
+          '';
+
+          nativeBuildInputs = with pkgs; [
+            go
+            gopls
+            golangci-lint
+          ];
+        };
+
+        checks = {
+          event-router = eventRouterPkg;
+          pre-commit = preCommitHook { doFullLint = true; };
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matthew-healy/event-router
+
+go 1.20
